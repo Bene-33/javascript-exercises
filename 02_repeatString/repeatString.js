358e7a4abfb03e521d7f3d537ce05d36d2c2be51
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-10-16 19:37:57 +0200

    while loop test is good, still need to concat the strings while loop running

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+let i = 0;
+while (i = rep){
+    return i;
+}
+    i++
 };
 
+
+repeatString("hey", 3)
 // Do not edit below this line
 module.exports = repeatString;
