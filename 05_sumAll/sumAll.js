bf8891255279570e32b79cdad6878ab50846a657
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-19 20:37:05 +0200

    first test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let sum = 0;
+    let array =[x,y];
+    let fullArray = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+    for(let i = array[0]; i <= array[1]; i++){
+        fullArray.push(i)
+
+    }
+
+    return sum = fullArray.reduce(reducer);
 };
 
 // Do not edit below this line
