4692bb27e07faff036a64dbacfb5c9b12ed75359
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-23 21:06:22 +0200

    set up if for normal logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let stringYear = year.toString().substr(-1, 2)
+    if( year % 4 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
