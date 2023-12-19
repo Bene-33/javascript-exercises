228adfe3f447f972de5d486873606b0d0c8a45ff
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-19 23:09:09 +0100

    first test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let string = word.toLowerCase();
+    let newString = string.split('').reverse().join('');
+    if(string === newString){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
