8de595777a4573b196f58c2525797dd4971d876e
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-28 17:28:03 +0100

    added function get calc the age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
 const findTheOldest = function() {
-
+  
+    let getAge = function(birth, death){
+        if (!death){
+            return new Date().getFullYear() - birth;
+        }
+        else {
+            return death - birth;
+        }
+    };
 };
 
 // Do not edit below this line
