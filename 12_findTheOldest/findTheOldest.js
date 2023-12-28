aebb8f71092cb4f73467d8585bfc27f8c9d5c22e
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-28 18:37:08 +0100

    first idea to solve it test with if

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,25 @@
-const findTheOldest = function() {
-  
-    let getAge = function(birth, death){
-        if (!death){
-            return new Date().getFullYear() - birth;
+const findTheOldest = function(array) {
+
+    return array.reduce((firstPerson, secondPerson) => {
+        let firstPersonAge = getAge(firstPerson.yearOfBirth, firstPerson.yearOfDeath);
+        let secondPersonAge = getAge(secondPerson.yearOfBirth, secondPerson.yearOfDeath);
+
+        if (firstPersonAge < secondPersonAge){
+            return secondPerson;
         }
-        else {
-            return death - birth;
+        else{ 
+            return firstPerson;
         }
-    };
+    },0);
 };
 
+let getAge = function(birth, death){
+    if (!death){
+        return new Date().getFullYear() - birth;
+    }
+    else {
+        return death - birth;
+    }
+};
 // Do not edit below this line
 module.exports = findTheOldest;
