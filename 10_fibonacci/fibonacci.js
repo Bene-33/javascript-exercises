bb2e4147769285e83c4eee25c31dd3c6c315587f
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-28 11:31:42 +0100

    first solution test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    let int = Number(value);
+    if(int <= 0){
+        return "OOPS";
+    }
+    else if(int <= 1){
+        return 1;
+    }
+    return fibonacci(int-1) + fibonacci(int-2);
 };
 
 // Do not edit below this line
