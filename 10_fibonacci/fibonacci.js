6b7bbd98546c4cb31b6dea6824cb00aedb90770f
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-28 12:36:59 +0100

    passed first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,18 @@
 const fibonacci = function(value) {
     let int = Number(value);
-    if(int < 0){
+    if (int <= 0){
         return "OOPS";
     }
-    else if(int < 1){
-        return int = 1;
+    else if (int == 1){
+        return 1;
     }
-    return (int-2) + (int-3);
+    else if (int == 2){
+        return 1;
+    }
+    else {
+        return fibonacci(int-1) + fibonacci(int-2)
+    }
+
 };
 
 // Do not edit below this line
